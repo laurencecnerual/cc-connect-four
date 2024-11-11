24db3e7cd23c2dd0ebac8df6f369351332f6e180
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-11-11 11:31:49 +0900

    Feat: Adds validation and game loop

--- connect-four.py
+++ connect-four.py
@@ -1,17 +1,24 @@
 board = [[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "]]
-gameOver = False
 
-#6hx7w
 def main():
+  gameOver = False
   print("Let the games begin!")
   print_board()
-  column = int(input("Choose a column number. Column chosen: "))
-  row = get_bottom(column)
-  if row >= 0 :
-    board[row][column] = "O"
-    print_board()
-  else:
-    print("That column is full. Try again")
+  while not gameOver:
+    isValidColumn = False
+    while not isValidColumn:
+      column = int(input("Choose a column number. Column chosen: "))
+      if column >= 0 and column <= 6:
+        isValidColumn = True
+      else:
+        print("Invalid selection. Try again.")
+    row = get_bottom(column)
+    if row >= 0:
+      board[row][column] = "O"
+      print_board()
+    else:
+      print("That column is full. Try again")
+      gameOver = True
 
 def print_board(): 
   print (" 0 1 2 3 4 5 6 ")
